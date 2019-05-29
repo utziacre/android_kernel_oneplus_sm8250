e16c2ff73683173c7ee3802828ef51fbb85d78fd
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2019-05-29 18:52:27 +0200

    workqueue: rework
    [ Upstream commit d15a862f24df983458533aebd6fa207ecdd1095a ]
    
    This is an all-in change of the workqueue rework.
    The worker_pool.lock is made to raw_spinlock_t. With this change we can
    schedule workitems from preempt-disable sections and sections with disabled
    interrupts. This change allows to remove all kthread_.* workarounds we used to
    have.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4789,6 +4789,7 @@ static inline void sched_submit_work(struct task_struct *tsk)
 {
 	if (!tsk->state)
 		return;
+
 	/*
 	 * If a worker went to sleep, notify and ask workqueue whether
 	 * it wants to wake up a task to maintain concurrency.
