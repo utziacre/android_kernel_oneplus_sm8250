e07161e93a38864164c79afa4be72208a8536c33
spakkkk <spakdroid@gmail.com>
2020-11-08 22:10:48 +0000

    kernel: sched: merge changes from LA.UM.9.12.R2.10.00.00.685.011
    https://github.com/aosp-mirror/kernel_msm/commit/56acc710a6df272771087971e69ccca13bf1cc68

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6760,7 +6760,7 @@ static inline int select_idle_sibling_cstate_aware(struct task_struct *p,
 					goto next;
 
 				/* figure out if the task can fit here at all */
-				new_usage = boosted_task_util(p);
+				new_usage = uclamp_task(p);
 				capacity_orig = capacity_orig_of(i);
 
 				if (new_usage > capacity_orig)
@@ -7031,8 +7031,8 @@ static void find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 	unsigned long best_active_util = ULONG_MAX;
 	unsigned long best_active_cuml_util = ULONG_MAX;
 	unsigned long best_idle_cuml_util = ULONG_MAX;
-	bool prefer_idle = schedtune_prefer_idle(p);
-	bool boosted;
+	bool prefer_idle = uclamp_latency_sensitive(p);
+	bool boosted = fbt_env->boosted;
 	/* Initialise with deepest possible cstate (INT_MAX) */
 	unsigned long best_idle_util = ULONG_MAX;
 	int shallowest_idle_cstate = INT_MAX;
@@ -7854,7 +7854,7 @@ static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu,
 	u64 start_t = 0;
 	int delta = 0;
 	int task_boost = per_task_boost(p);
-	int boosted = (schedtune_task_boost(p) > 0) || (task_boost > 0);
+	int boosted = uclamp_boosted(p);
 	int start_cpu = get_start_cpu(p, sync_boost);
 
 	if (start_cpu < 0)
@@ -10833,14 +10833,10 @@ static int load_balance(int this_cpu, struct rq *this_rq,
 		.loop		= 0,
 	};
 
-#ifdef CONFIG_SCHED_WALT
 	env.prefer_spread = (idle != CPU_NOT_IDLE &&
 				prefer_spread_on_idle(this_cpu) &&
 				!((sd->flags & SD_ASYM_CPUCAPACITY) &&
 				 !is_asym_cap_cpu(this_cpu)));
-#else
-	env.prefer_spread = false;
-#endif
 
 	cpumask_and(cpus, sched_domain_span(sd), cpu_active_mask);
 
