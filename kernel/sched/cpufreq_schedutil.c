338c277e836a11b77d06019d162b3df32f1e1647
Xuewen Yan <xuewen.yan@unisoc.com>
2023-07-19 21:05:27 +0800

    cpufreq: schedutil: Update next_freq when cpufreq_limits change
    When cpufreq's policy is 'single', there is a scenario that will
    cause sg_policy's next_freq to be unable to update.
    
    When the CPU's util is always max, the cpufreq will be max,
    and then if we change the policy's scaling_max_freq to be a
    lower freq, indeed, the sg_policy's next_freq need change to
    be the lower freq, however, because the cpu_is_busy, the next_freq
    would keep the max_freq.
    
    For example:
    
    The cpu7 is a single CPU:
    
      unisoc:/sys/devices/system/cpu/cpufreq/policy7 # while true;do done& [1] 4737
      unisoc:/sys/devices/system/cpu/cpufreq/policy7 # taskset -p 80 4737
      pid 4737's current affinity mask: ff
      pid 4737's new affinity mask: 80
      unisoc:/sys/devices/system/cpu/cpufreq/policy7 # cat scaling_max_freq
      2301000
      unisoc:/sys/devices/system/cpu/cpufreq/policy7 # cat scaling_cur_freq
      2301000
      unisoc:/sys/devices/system/cpu/cpufreq/policy7 # echo 2171000 > scaling_max_freq
      unisoc:/sys/devices/system/cpu/cpufreq/policy7 # cat scaling_max_freq
      2171000
    
    At this time, the sg_policy's next_freq would stay at 2301000, which
    is wrong.
    
    To fix this, add a check for the ->need_freq_update flag.
    
    [ mingo: Clarified the changelog. ]
    
    Co-developed-by: Guohua Yan <guohua.yan@unisoc.com>
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>
    Signed-off-by: Guohua Yan <guohua.yan@unisoc.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: "Rafael J. Wysocki" <rafael@kernel.org>
    Link: https://lore.kernel.org/r/20230719130527.8074-1-xuewen.yan@unisoc.com

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -427,7 +427,7 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 	 * recently, as the reduction is likely to be premature then.
 	 */
 	if (busy && next_f < sg_policy->next_freq &&
-	    sg_policy->next_freq != UINT_MAX) {
+	    sg_policy->next_freq != UINT_MAX && !sg_policy->need_freq_update) {
 		next_f = sg_policy->next_freq;
 
 		/* Restore cached freq as next_freq has changed */
