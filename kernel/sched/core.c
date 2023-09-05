73c16cff2714f1bdc43d86b8929769119b0db12b
EmanuelCN <emanuelghub@gmail.com>
2023-09-05 16:31:37 +0000

    kernel/sched: Cover more walt cases

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4249,6 +4249,7 @@ void scheduler_tick(void)
 	trigger_load_balance(rq);
 #endif
 
+#ifdef CONFIG_SCHED_WALT
 	rcu_read_lock();
 	grp = task_related_thread_group(curr);
 	if (update_preferred_cluster(grp, curr, old_load, true))
@@ -4264,7 +4265,7 @@ void scheduler_tick(void)
 		clear_reserved(cpu);
 	rq_unlock(rq, &rf);
 #endif
-
+#endif
 }
 
 #ifdef CONFIG_NO_HZ_FULL
