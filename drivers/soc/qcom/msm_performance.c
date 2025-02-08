a85ab6d633f3fc6bb730ff1d5880d5138cea6f60
utziacre <utziacre@gmail.com>
2025-02-08 12:50:24 +0800

    msm_performance: Block perfd from changing min / max frequency

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -18,6 +18,7 @@
 #include <linux/input.h>
 #include <linux/kthread.h>
 #include <linux/sched/core_ctl.h>
+#include <linux/binfmts.h>
 
 /*
  * Sched will provide the data for every 20ms window,
@@ -63,6 +64,9 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	struct cpu_status *i_cpu_stats;
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
+
+	if (task_is_booster(current))
+		return 0;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
@@ -142,6 +146,9 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	struct cpu_status *i_cpu_stats;
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
+
+        if (task_is_booster(current))
+                return 0;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
